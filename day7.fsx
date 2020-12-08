ed1ddc14ff42e4a9d8c1ed36514fc5a643228cd1
Väinö Jalonen <vaino.jalonen@gmail.com>
2020-12-08 16:16:51 +0200

    day 7 improvements

--- day7.fsx
+++ day7.fsx
@@ -12,51 +12,49 @@ let bags =
   input.Split ([|"\n"|],  StringSplitOptions.RemoveEmptyEntries)
   |> Seq.toList
   |> List.map(fun e -> 
-    { Color = Regex.Match( e, @"([\w]+\s+){2}").Value.Trim()
+    let matches = 
+      Regex.Matches( e, @"(^([\w]+\s+){2})|(\d ([\w]+\s+){2})")
+      |> Seq.cast<Match>
+      |> Seq.map (fun e -> e.Value)
+
+    { Color = (matches |> Seq.head).Trim()
       Content = 
-        Regex.Matches(e, @"(\d.+?)(?:bags?\s*|$)")
-        |> Seq.cast<Match>
-        |> Seq.toList
-        |> List.map(fun m -> 
-          Regex.Match(m.Value.Trim(), @"([\w]+\s+){3}").Value.Substring(2).Trim(), int (m.Value.Substring(0, 1)))})
+        matches 
+        |> Seq.tail
+        |> Seq.map(fun e -> e.Substring(2).Trim(), int(e.[0]))
+        |> Seq.toList })
 
 let getBag color = 
   bags
   |> List.filter (fun bag -> bag.Color = color)
   |> List.exactlyOne
 
-let rec canHoldShinyGold bag = 
-  let canHoldDirectly = 
-    bag.Content
-    |> List.exists(fun e -> fst e = "shiny gold")
-  if canHoldDirectly 
-    then true
+let rec canHoldShinyGold bag =     
+  if List.exists(fun (color, _) -> color = "shiny gold") bag.Content
+  then true
   else 
     bag.Content
-    |> List.map (fun e -> getBag(fst e))
-    |> List.exists(fun e -> canHoldShinyGold e)
+    |> List.exists(fun (color, _) -> 
+      color 
+      |> getBag 
+      |> canHoldShinyGold)
 
 let ``bags that can hold shiny gold`` = 
   bags 
   |> List.filter canHoldShinyGold 
   |> List.length
 
+printf "Answer to part 1: %i \n" ``bags that can hold shiny gold``
+
+// part 2
+
 let rec countContainedBags bag : int =
-  let containedDirectly = 
-    bag.Content
-    |> List.sumBy (fun e -> snd e)
-  let containedByInner = 
-    bag.Content
-    |> List.map(fun e -> getBag (fst e), snd e)
-    |> List.sumBy(fun e -> countContainedBags (fst e) * (snd e))
-  containedDirectly + containedByInner
+  bag.Content
+  |> List.sumBy(fun (color, count) -> count + countContainedBags (getBag color) * count)
   
 let shinyGold = 
   bags
   |> List.filter(fun b -> b.Color = "shiny gold")
   |> List.exactlyOne
 
-let ``bags inside the shiny gold`` = countContainedBags shinyGold
-
-printf "Answer to part 1: %i \n" ``bags that can hold shiny gold``
-printf "Answer to part 2: %i \n" ``bags inside the shiny gold``
+printf "Answer to part 2: %i \n" countContainedBags shinyGold
