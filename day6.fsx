8dae673a8cc30dce737cec42b06cf9a0120d28c2
Väinö Jalonen <vaino.jalonen@gmail.com>
2020-12-08 16:17:17 +0200

    day 6 improvements

--- day6.fsx
+++ day6.fsx
@@ -16,14 +16,16 @@ let part2 =
   input.Split ([|"\n\n"|],  StringSplitOptions.RemoveEmptyEntries)
   |> Seq.toList
   |> List.map (fun group -> 
-    let lines = 
-      group.Split ([|"\n"|],  StringSplitOptions.RemoveEmptyEntries)
-      |> Seq.toList
-      |> List.map(fun e -> e.ToCharArray() |> Seq.toList)
     group.ToCharArray()
     |> Seq.toList
     |> List.distinct
-    |> List.filter (fun i -> ['a' .. 'z'] |> List.contains i)
-    |> List.filter (fun answer -> lines |> List.forall(fun e -> e |> List.contains answer))
+    |> List.filter (fun e -> ['a' .. 'z'] |> List.contains e)
+    |> List.filter (fun answers -> 
+      group.Split ([|"\n"|],  StringSplitOptions.RemoveEmptyEntries)
+      |> Seq.toList
+      |> List.forall(fun e -> 
+        e.ToCharArray() 
+        |> Seq.toList 
+        |> List.contains answers))
     |> List.length)
   |> List.sum
